a90b71e851d2b288f5f181f71513ba12d13500c5
maxconsul17 <mconsul@schools.ph>
2025-01-27 13:04:21 +0800

    additional bug fix

--- application/controllers/Que.php
+++ application/controllers/Que.php
@@ -195,16 +195,9 @@ class Que extends CI_Controller {
 				$data["report_list"] = [["report" => $report]];
 				$data["path"] = "files/reports/pdf/{$employee->employeeid}_{$det->id}.pdf";
 
-                // ADDITIONAL TRY CATCH FOR ERROR HANDLING
-                try {
-                    // Save the report breakdown and generate the PDF
-                    $this->worker_model->save_report_breakdown($report_data);
-                    $this->load->view('dtr/daily_time_report_pdf', $data);
-                }catch (Exception $e) {
-                    $this->worker_model->updateReportStatus($det->id, "", "error encountered");
-                    continue;
-                }
-
+				// Save the report breakdown and generate the PDF
+				$this->worker_model->save_report_breakdown($report_data);
+				$this->load->view('dtr/daily_time_report_pdf', $data);
 			}catch (Exception $e) {
                 $this->worker_model->updateReportStatus($det->id, "", "error encountered");
 				continue;
