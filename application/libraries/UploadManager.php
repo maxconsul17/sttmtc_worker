610af0bab6f0a90abdfcc1843b43a4ee915ebb43
maxconsul17 <mconsul@schools.ph>
2025-07-30 18:28:33 +0800

    for checking

--- application/libraries/UploadManager.php
+++ application/libraries/UploadManager.php
@@ -13,16 +13,8 @@ class UploadManager
         $this->CI = & get_instance();
         
         $this->CI->load->model("Worker_model", "worker_model");
-        $this->CI->load->model("Time", "time");
-        $this->CI->load->model("employeeAttendance", "employeeAttendance");
-        $this->CI->load->model("Employee", "employee");
-        $this->CI->load->model("Facial", "facial");
 
         $this->worker_model = $this->CI->worker_model;
-        $this->time = $this->CI->time;
-        $this->employeeAttendance = $this->CI->employeeAttendance;
-        $this->employee = $this->CI->employee;
-        $this->facial = $this->CI->facial;
     }
 
     public function getUploadJob()
